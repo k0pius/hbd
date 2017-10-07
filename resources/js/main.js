196574adbb4dca1c2a19f651d216d190ab829bd2
k0pius <hereyesprevail@gmail.com>
2017-10-07 09:04:16 -0300

    first draft..hopefully not last

---
+++ resources/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready( function() {
+  $('.yes').mousedown(function(){
+    $('.question').fadeOut(2000, function() {
+      $('.options').fadeOut(0);
+      $('.omg').addClass('on');
+      $('.heart').removeClass('off');
+      $('.omgtext').delay(4000).fadeOut(2000, function() {
+        var hbd = document.getElementById('hbd');
+        hbd.play();
+        $('.b').removeClass('off').addClass('banim');
+      });
+
+    });
+  });
+});
