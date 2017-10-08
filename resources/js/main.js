1c778a3e35c9279957d0615878a267dbdcf38554
k0pius <hereyesprevail@gmail.com>
2017-10-08 10:03:12 -0300

    ok like 65% there

--- resources/js/main.js
+++ resources/js/main.js
@@ -4,10 +4,12 @@ $(document).ready( function() {
       $('.options').fadeOut(0);
       $('.omg').addClass('on');
       $('.heart').removeClass('off');
+      $('.g').delay(6000).removeClass('off').addClass('ganim');
       $('.omgtext').delay(4000).fadeOut(2000, function() {
         var hbd = document.getElementById('hbd');
         hbd.play();
         $('.b').removeClass('off').addClass('banim');
+        $('.happytext').delay(20000).fadeIn(1000);
       });
 
     });
